4a076f5a9c477f46a6af99240e698724bba78c07
Frida Sjoholm <frida@fridaesign.se>
2017-03-17 09:59:34 -0700

    Adds markup for material and refactors for simplicity

--- markup.rb
+++ markup.rb
@@ -1,17 +1,31 @@
 def markup(original_cost, people, materials)
 
   new_cost = flat_markup(original_cost)
-  new_cost = person_based_markup(new_cost, people)
-  return new_cost
+  additional_markup =  material_based_markup(materials) + person_based_markup(people)
+
+ return (new_cost *(1 + additional_markup)).round(2)
 end
 
 def flat_markup(current_cost)
-  return current_cost * 1.05
+  current_cost * 1.05
 end
 
-def person_based_markup(current_cost, people)
-  markup = 1 + (0.012 * people)
-  return current_cost * markup
+def person_based_markup(people)
+  0.012 * people
+end
+
+def material_based_markup(materials)
+  if materials == "food"
+    return 0.13
+  elsif materials == "drugs"
+    return 0.075
+  elsif materials == "electronics"
+    return 0.02
+  else
+    return 0
+  end
 end
 
 p markup(1299.99, 3, "food")
+p markup(5432.00, 1, "drugs")
+p markup(12456.95, 4, "books")
